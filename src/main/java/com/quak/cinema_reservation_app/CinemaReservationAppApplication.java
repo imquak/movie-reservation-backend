cc65a3f10e7a784478ffc78b8494619890630d89
Afonso Pinto <patinhofeiox@gmail.com>
2025-10-30 14:46:22 +0000

    Created Repository

---
+++ src/main/java/com/quak/cinema_reservation_app/CinemaReservationAppApplication.java
@@ -0,0 +1,13 @@
+package com.quak.cinema_reservation_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaReservationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaReservationAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quak/cinema_reservation_app/CinemaReservationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quak.cinema_reservation_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaReservationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
